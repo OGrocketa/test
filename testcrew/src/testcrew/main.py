f34e2287744f8dc1a2b87432fb6bdc75b4b58095
Yaraslau Sedach <jaroslaw.sedach@gmail.com>
2025-03-01 12:57:07 +0100

    feat: gradio, add cusstom tool for pdf

--- testcrew/src/testcrew/main.py
+++ testcrew/src/testcrew/main.py
@@ -1,20 +1,45 @@
 from testcrew.crew import Testcrew
+import gradio as gr
+import shutil, os
+
+
+
+
+def chat(user_message, history):
+    response = crew_instance.kickoff({"user_question": user_message})
+    return response
+
+def process_file(uploaded_files):
+    pdf_dir = "../../../knowledge"
+    for uploaded_file in uploaded_files:
+        file_name = os.path.basename(uploaded_file)
+       
+        dest_path = os.path.join(pdf_dir, file_name)
+        
+        shutil.copyfile(uploaded_file, dest_path)
+    return "Files uploaded successfully you can start asking questions now"
+
+crew_instance = Testcrew().crew()
+
+with gr.Blocks() as demo:
+
+    file_upload = gr.Interface(
+        fn = process_file,
+        inputs = gr.File(label="Upload PDFs", file_types=[".pdf"], file_count="multiple"),
+        outputs = gr.Textbox(value="Submit PDF's to ask about them", label="Upload Status")
+    )
+
+    chat_interface = gr.ChatInterface(
+        fn = chat,
+        title = "PDF Chatbot",
+        description = "Ask questions about your PDF",
+        type = "messages",
+        save_history = True
+    )
 
 def run():
     """
     Run the crew.
     """
-    print("\n=== CrewAI Chat Interface ===")
-    crew_instance = Testcrew().crew()
-
-    while True:
-        user_input = input("\n> ")
-        if user_input.lower() in ["exit", "quit", "e"]:
-            print("Goodbye!")
-            break
-
-        if not user_input.strip():
-            continue
-
-        response = crew_instance.kickoff({"user_question":user_input})
-        print(f"\n{response}")
+    demo.launch()
+   
